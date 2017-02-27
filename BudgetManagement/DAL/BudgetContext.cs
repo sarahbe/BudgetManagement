f3ba50eb018bb0461986940dd5c45e8640e62272
Serah BEIRKDAR <Sarah.Beirkdar@profe.local>
2017-02-27 10:12:49 +0300

    add context

---
+++ BudgetManagement/DAL/BudgetContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using BudgetManagement.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace BudgetManagement.DAL
+{
+    public class BudgetContext : DbContext
+
+    {
+        //This is the name of connection string
+     public BudgetContext() : base("BudgetContext")
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Right> Rights { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<TransactionType> TransactionTypes { get; set; }
+
+        //preventint table names from bein pluralized
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
