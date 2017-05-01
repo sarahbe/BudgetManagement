4620a4b7b4bc654ffff6b40c47425c763fb12c6e
Sarah MAC <sarah.beirkdar@gmail.com>
2017-05-01 21:06:30 +0300

    add account right domain

---
+++ BudgetManagement/Domain/AccountRight.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BudgetManagement.Domain
+{
+    public class AccountRight:BaseEntity
+    {
+        public string UserID { get; set; }
+        public bool FlWrite { get; set; }
+    }
+}
