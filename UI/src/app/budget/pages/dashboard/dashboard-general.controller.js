a31d1bcf7589df820100f146b1aaaf5da3ed302e
sarahbe <sarah.beirkdar@gmail.com>
2017-05-12 20:54:13 +0300

    dashboard UI

--- UI/src/app/budget/pages/dashboard/dashboard-general.controller.js
+++ UI/src/app/budget/pages/dashboard/dashboard-general.controller.js
@@ -8,6 +8,41 @@
     /* @ngInject */
     function DashboardGeneralController($state, $filter) {
         var vm = this;
-        vm.name = "sarah";
+        vm.init = init;
+
+        init();
+
+        function init() {
+            vm.cardStats = {};
+            vm.cardStats.expenseToday = 150;
+            vm.cardStats.incomeToday = 450;
+            vm.cardStats.balance = 3200;
+
+            vm.series = ['Income', 'Expense'];
+            vm.labels = ['1', '2', '3', '4'];
+            vm.options = {
+                datasetFill: false
+            };
+            vm.data = [
+                [45, 150, 500, 10],
+                [100, 125, 50, 110]
+            ];
+
+
+            vm.pieLabels = ['Shopping', 'Oil', 'Bills', 'Education'];
+            vm.pieOptions = {
+                datasetFill: false
+            };
+            vm.pieData = [25,15,35,25];
+
+
+            vm.barSeries = ['Income', 'Expense'];
+            vm.barLabels = ['15-25', '25-35', '35-50', '50-70'];
+            vm.barData = [
+                [45, 150, 500, 10],
+                [100, 125, 50, 110]
+            ];
+        }
+
     }
 })();
